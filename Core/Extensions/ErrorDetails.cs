14fb30d991316fbfdddc9b3d56bc12566108a600
Mehmet Akif Şanlan <135071851+makifsanlan@users.noreply.github.com>
2025-03-11 16:49:02 +0300

    Extentions ile frontend kısmıyla validation işlemlerinin düzenlenmesi

--- Core/Extensions/ErrorDetails.cs
+++ Core/Extensions/ErrorDetails.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,17 @@ namespace Core.Extensions
     public class ErrorDetails
     {
         public string Message { get; set; }
+
         public int StatusCode { get; set; }
 
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
         }
+    }
+
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
     }
 }
